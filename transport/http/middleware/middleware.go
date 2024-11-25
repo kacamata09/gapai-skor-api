2ccb03d7857516312fe4c3a3169c25de1b4e6fc8
Muh. Anshar Azhari <muh.ansharazhari@gmail.com>
2024-11-25 09:52:58 +0700

    feat: update middleware

--- transport/http/middleware/middleware.go
+++ transport/http/middleware/middleware.go
@@ -10,7 +10,16 @@ type Middleware struct {
 func (m *Middleware) CORS(next echo.HandlerFunc) echo.HandlerFunc {
 	return func(c echo.Context) error {
 
-		c.Response().Header().Set("Access-Control-Origin-Allow", "*")
+		c.Response().Header().Set("Access-Control-Allow-Origin", "*") // Mengizinkan semua origin
+		c.Response().Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		c.Response().Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		c.Response().Header().Set("Access-Control-Allow-Credentials", "true") // Jika perlu kredensial
+
+		// Tangani preflight OPTIONS request
+		if c.Request().Method == "OPTIONS" {
+			return c.NoContent(204) // Mengembalikan status 204 untuk preflight
+		}
+
 		return next(c)
 	}
 }
