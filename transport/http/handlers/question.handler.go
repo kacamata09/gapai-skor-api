3646d759cbbd81bb6f1d753f09bd20ea0d206e61
Muh. Anshar Azhari <muh.ansharazhari@gmail.com>
2024-12-05 13:41:42 +0700

    feat: add upload handler in question

--- transport/http/handlers/question.handler.go
+++ transport/http/handlers/question.handler.go
@@ -3,9 +3,13 @@ package handler
 import (
 	// "fmt"
 
+	"fmt"
 	"gapai-skor-api/domain"
 	helper_http "gapai-skor-api/transport/http/helper"
 	"net/http"
+	"os"
+	"path/filepath"
+	"time"
 
 	// "strconv"
 
@@ -29,6 +33,7 @@ func QuestionRoute(e *echo.Echo, uc domain.QuestionUsecase) {
 	e.POST("/question_options", handler.CreateWithAnswerOptions)
 	e.GET("/question/:id", handler.GetByIDHandler)
 	e.GET("/question/test_id/:id", handler.GetByTestIDHandler)
+	e.POST("/upload", handler.UploadFile)
 
 }
 
@@ -113,4 +118,56 @@ func (h *QuestionHandler) CreateWithAnswerOptions(c echo.Context) error {
 	}
 
 	return helper_http.SuccessResponse(c, data, "success create question with answer_options")
+}
+
+type FileUpload struct {
+	File string `json:"file"`
+}
+
+// UploadFile handles the file upload
+func (h *QuestionHandler) UploadFile(c echo.Context) error {
+	// Mendapatkan file dari form data
+	file, err := c.FormFile("file")
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, fmt.Sprintf("failed to get file: %v", err))
+	}
+
+	// Tentukan lokasi penyimpanan file
+	uploadDir := "../gapaiskor_fe/build/uploads"
+	// Tentukan lokasi penyimpanan file
+	// uploadDir := "/home/gapaisko/gapaiskorweb/gapaiskor_fe/build/uploads"
+
+	if err := os.MkdirAll(uploadDir, os.ModePerm); err != nil {
+		return c.JSON(http.StatusInternalServerError, fmt.Sprintf("failed to create upload directory: %v", err))
+	}
+
+	// Menambahkan timestamp atau UUID untuk menghindari penimpaan file yang sama
+	uniqueFilename := fmt.Sprintf("%d_%s", time.Now().Unix(), file.Filename)
+	dst := filepath.Join(uploadDir, uniqueFilename)
+
+	// Membuka file tujuan untuk menulis
+	src, err := file.Open()
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, fmt.Sprintf("failed to open file: %v", err))
+	}
+	defer src.Close()
+
+	// Membuat file di tujuan dan menulis file
+	dstFile, err := os.Create(dst)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, fmt.Sprintf("failed to create file: %v", err))
+	}
+	defer dstFile.Close()
+
+	// Menyalin file ke lokasi tujuan
+	_, err = dstFile.ReadFrom(src)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, fmt.Sprintf("failed to save file: %v", err))
+	}
+
+	dst = "https://gapaiskor.web.id/uploads/" + uniqueFilename
+	// Mengembalikan response sukses dengan lokasi file yang di-upload
+	// return c.JSON(http.StatusOK, helper_http.SuccessResponse(c, FileUpload{File: dst}, "file uploaded successfully"))
+	return helper_http.SuccessResponse(c, FileUpload{File: dst}, "success file upload")
+
 }
